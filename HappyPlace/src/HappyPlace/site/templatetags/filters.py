e1696a8f804a5e1f0c90766d4a6acd8b7f1d39c7
Xavier <Xavier.owens0@gmail.com>
2016-04-02 20:26:04 -0400

    Signed-off-by: Xavier <Xavier.owens0@gmail.com>

--- HappyPlace/src/HappyPlace/site/templatetags/filters.py
+++ HappyPlace/src/HappyPlace/site/templatetags/filters.py
@@ -6,6 +6,8 @@ register = template.Library()
 @register.filter(name='stripHttp')
 @stringfilter
 def stripHttp(value):
-    value = value[7:]
-    return value if not value.startswith('www') else value[4:]
+    value = value[7:] #strip http://
+    value = value if not value.startswith('www') else value[4:] #strip www
+    value = value if not value.endswith('/') else value[:len(value)-1] #strip trailing slash
+    return value
 
