12f294a7941d797ce03e4398377981eb3dd7c7c6
Xavier <Xavier.owens0@gmail.com>
2016-04-09 19:04:54 -0400

    Signed-off-by: Xavier <Xavier.owens0@gmail.com>

--- HappyPlace/src/HappyPlace/urls.py
+++ HappyPlace/src/HappyPlace/urls.py
@@ -2,8 +2,9 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-urlpatterns = [ patterns('',
+urlpatterns =  patterns('',
     # Examples:
     # url(r'^$', 'HappyPlace.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
@@ -14,5 +15,5 @@ urlpatterns = [ patterns('',
     url(r'^submit', 'HappyPlace.site.views.AddHappyPlace', name='add'),
     url(r'^submit/', 'HappyPlace.site.views.AddHappyPlace', name='add'),
     url(r'^', 'HappyPlace.site.views.Home', name='home'),
-) ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+) + staticfiles_urlpatterns()
 
